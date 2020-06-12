32baa69a511d05c8eedfb5fcd0c3e9f6d6b108fb
Sergey Troyeglazov <troyeglazov.s.a@gmail.com>
2020-06-12 15:12:04 -0400

    Page Object Structure

--- test/cca/AppPage.js
+++ test/cca/AppPage.js
@@ -2,4 +2,16 @@ class AppPage {
   open(){
     browser.url('https://likejean.github.io/homework-5/')
   }
+
+  //method - delete counter by id
+
+  //method - add new counter (name, initial)
+
+  //method - reset counter / via $$[number] ??
+
+  //count negative buttons - check by text???
+
+  //count posititve - check by text ???
+
+
 }
