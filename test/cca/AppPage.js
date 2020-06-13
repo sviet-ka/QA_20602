a7d6a9f148459505f5b0a2eefb6a47f7ee24088a
rinsal <notthepeterpig@gmail.com>
2020-06-12 18:55:58 -0700

    rinat test added

--- test/cca/AppPage.js
+++ test/cca/AppPage.js
@@ -1,7 +1,5 @@
 class AppPage {
-  open(){
-    browser.url('https://likejean.github.io/homework-5/')
-  }
+
 
   get mainHeader () {
     return $('h1');
@@ -13,12 +11,30 @@ class AppPage {
   get defaultCounterName(){
     return $$('h3')[1];
   }
-  counterWithIDExist (i) {
-        $(`//h3[text()="${i}. "]`).isDisplayed();
-  };
 
 
+  get leftPlaceholder() {
+    return $('button[name="negative"]');
+    }
 
+  get rightPlaceholder() {
+    return $('button[name="positive"]');
+  }
+
+  get leftPlaceholders() {
+    return $$('button[name="negative"]');
+  }
+
+  get rightPlaceholders() {
+    return $$('button[name="positive"]');
+  }
+
+  get blackButton() {
+    return $('button[class*="btn-black"]');
+  }
+  get blackButtons() {
+    return $$('button[class*="btn-black"]');
+  }
 
 
 
@@ -31,8 +47,16 @@ class AppPage {
   //count negative buttons - check by text???
 
   //count posititve - check by text ???
+  open(){
+    browser.url('https://likejean.github.io/homework-5/')
+  }
+  counterWithIDExist (i) {
+    $(`//h3[text()="${i}. "]`).isDisplayed();
+  };
 
-
+  clickBlackButton (str) {
+    $(`//button[text()="${str}"]`).click();
+  }
 }
 
 export default new AppPage();
